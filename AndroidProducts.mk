3cef105d78b9485cb175284ceed800d7c7136b75
Soft-Bullet <binaytiwari6@gmail.com>
2019-08-14 20:38:00 +0545

    a5ultexx: Removed Doze and misc staffs

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,3 @@
 LOCAL_PATH := device/samsung/a5ultexx
 PRODUCT_MAKEFILES := \
-        $(LOCAL_DIR)/lineage_a5ultexx.mk \
-        $(LOCAL_DIR)/viper_a5ultexx.mk \
-        $(LOCAL_DIR)/havoc_a5ultexx.mk \
-        $(LOCAL_DIR)/aosp_a5ultexx.mk
+        $(LOCAL_DIR)/lineage_a5ultexx.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo aosp_a5ultexx-${i}
+	add_lunch_combo lineage_a5ultexx-${i}
 done
